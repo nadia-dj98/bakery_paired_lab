6270b11950bde572b404774f7ac800a3ca3c95d2
Nadia <nadia_dj@hotmail.co.uk>
2023-01-23 16:26:01 +0000

    completed mvp

---
+++ src/CakeForm.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+
+const CakeForm = ( {addNewRecipe} ) => {
+
+    const [cakeName, setCakeName] = useState("");
+    const [ingredients, setIngredients] = useState("");
+    const [rating, setRating] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newRecipe = {
+            cakeName : cakeName,
+            ingredients : ingredients,
+            rating: rating
+        }
+        addNewRecipe(newRecipe);
+    
+        
+    };
+
+    
+    return (
+    <div>
+        <h2>Add your recipe!</h2>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Cake Name:</label>
+            <input type="text" id="name" 
+                    
+                    placeholder='Name of your cake'
+                    onChange={(event)=> setCakeName(event.target.value)}
+                    value={cakeName}/>
+
+            <label htmlFor="ingredients">Ingredients:</label>
+            <textarea cols="30" rows="1" id="ingredients"
+                        
+                        placeholder="Your ingredients"
+                        onChange={(event)=> setIngredients(event.target.value)}
+                        value={ingredients}></textarea>
+            
+            <label htmlFor="rating">Rating:</label>
+            <input type="number" id="rating" min={1} max={5}
+                    
+                    onChange={(event)=> setRating(event.target.value)}
+                    value={rating}/>
+            
+            <input type="submit"/>
+        </form>
+    
+    </div> 
+    )
+
+}
+
+export default CakeForm;
